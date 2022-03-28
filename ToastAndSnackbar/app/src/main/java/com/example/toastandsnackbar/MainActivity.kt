296a7b911b1bdc075b44065ff5e8baebe9a1d98d
ardanvf <71808926+ardanvf@users.noreply.github.com>
2022-03-28 22:31:29 +0700

    Snackbar
    Only 1 button :anguished:

--- ToastAndSnackbar/app/src/main/java/com/example/toastandsnackbar/MainActivity.kt
+++ ToastAndSnackbar/app/src/main/java/com/example/toastandsnackbar/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.toastandsnackbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.toastandsnackbar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            if (binding.editText    .text.isNullOrEmpty()){
+                Toast.makeText(this, "Masukkan Nama Anda", Toast.LENGTH_LONG).show()
+            } else {
+                val snack = Snackbar.make(it, "Contoh Snekbar", Snackbar.LENGTH_INDEFINITE)
+                snack.setAction("Tombol1") {
+                    Toast.makeText(this, "Toast dari Action Snackbar", Toast.LENGTH_LONG).show()
+                }.show()
+            }
+        }
+
     }
 }
