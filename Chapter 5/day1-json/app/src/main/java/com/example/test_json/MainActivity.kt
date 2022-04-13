d2d98d6dcbe5fa3f276aff2ae3c33309f3d0a371
ardanvf <71808926+ardanvf@users.noreply.github.com>
2022-04-13 11:26:59 +0700

    Update Selesaii

--- Chapter 5/day1-json/app/src/main/java/com/example/test_json/MainActivity.kt
+++ Chapter 5/day1-json/app/src/main/java/com/example/test_json/MainActivity.kt
@@ -16,11 +16,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val jsonArray = JSONArray()
         val jsonObject = JSONObject()
         jsonObject.put("status", "OK")
         jsonObject.put("message", "Success")
 
+
+        val jsonArray = JSONArray()
         val jsonBook = JSONObject()
             .put("name", "Book")
             .put("price",100000)
@@ -31,11 +32,12 @@ class MainActivity : AppCompatActivity() {
 
         val jsonCandle = JSONObject()
             .put("name", "Candle")
-            .put("Price", 150000)
+            .put("price", 150000)
             .put("code", "CD120103929")
             .put("discount", 70000)
             .put("qty", 2)
             .put("new", true)
+        //Array Detail
         jsonArray.put(jsonBook)
         jsonArray.put(jsonCandle)
 
@@ -53,30 +55,45 @@ class MainActivity : AppCompatActivity() {
 
         //data
         jsonObject.put("data", jsonData)
-        jsonObject.put("detail", jsonArray)
+//        jsonData.put("detail", jsonArray)
+//        jsonObject.put("detail", jsonArray)
         //receipt
         jsonObject.put("receipt", jsonReceipt)
 
-        val nameToko = jsonObject.getJSONObject("receipt").getJSONObject("store")
+        val nameToko = jsonObject.getJSONObject("receipt").getString("store")
         val lokasiToko = jsonObject.getJSONObject("receipt").getString("address")
         val noteToko = jsonObject.getJSONObject("receipt").getString("add_note")
 
-        val price = jsonObject.getJSONObject("data").getJSONArray("detail").getJSONObject(0).getInt("price")
- //       val price2 = jsonObject.getJSONObject("data").getJSONArray("detail").getJSONObject(1).getInt("price")
-//        val barang1 = jsonObject.getJSONObject("data").getJSONArray("detail").getJSONObject(0).getString("name")
-//        val barang2 = jsonObject.getJSONObject("data").getJSONArray("detail").getJSONObject(1).getString("name")
-//        val qty1 = jsonObject.getJSONObject("data").getJSONArray("detail").getJSONObject(0).getInt("qty")
-//        val qty2 = jsonObject.getJSONObject("data").getJSONArray("detail").getJSONObject(1).getInt("qty")
+        val price = jsonData.getJSONArray("detail").getJSONObject(0).getInt("price")
+        val price2 = jsonData.getJSONArray("detail").getJSONObject(1).getInt("price")
+        val barang1 = jsonData.getJSONArray("detail").getJSONObject(0).getString("name")
+        val barang2 = jsonData.getJSONArray("detail").getJSONObject(1).getString("name")
+        val qty1 = jsonData.getJSONArray("detail").getJSONObject(0).getInt("qty")
+        val qty2 = jsonData.getJSONArray("detail").getJSONObject(1).getInt("qty")
+        val discount = jsonData.getJSONArray("detail").getJSONObject(1).getInt("discount")
+        val hasilDiscount = price2-discount
+        val total = price + hasilDiscount
+        val bayar = 200000
+        val kembali = bayar - total
+
 
 
         binding.textView.text = nameToko.toString()
         binding.textView2.text = lokasiToko.toString()
         binding.textView3.text = noteToko.toString()
 
-//        binding.textView4.text = barang1.toString()
-//        binding.textView5.text = barang2.toString()
+        binding.textView4.text = barang1.toString()
+        binding.textView5.text = barang2.toString()
         binding.textView7.text = price.toString()
-//        binding.textView8.text = price2.toString()
+        binding.textView8.text = hasilDiscount.toString()
+        binding.textView14.text = " x ${qty1.toString()}"
+        binding.textView15.text = " x ${qty2.toString()}"
+
+        binding.textView9.text = total.toString()
+        binding.textView16.text = bayar.toString()
+        binding.textView17.text = kembali.toString()
 
     }
 }
+
+
